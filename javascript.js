566a9725ed2edf1bba21156eae24e6a984fa6d20
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-03-26 18:07:00 -0400

    Add basic arithmetic functions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const a = 5;
+const b = 2;
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
