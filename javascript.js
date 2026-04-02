3eeadcded7097c4a25abf30d038d7a5a591c7260
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-04-02 17:17:42 -0400

    Impliment partially working logic

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
+const button = document.querySelector(".calculator");
+const screen = document.querySelector(".screen");
+
 function add(a, b) {
+    const result = Number(a) + Number(b)
+    updateScreen(result);
     return a + b;
 }
 
@@ -34,10 +39,65 @@ function operate(operator, a, b) {
     }
 }
 
-const a = 5;
-const b = 2;
+function updateScreen(text) {
+    if (text == "") {
+        screen.textContent = ""
+    } else {
+        screen.textContent += text;
+    }
+}
 
-console.log(add(a,b));
-console.log(subtract(a,b));
-console.log(multiply(a,b));
-console.log(divide(a,b));
+function clear() {
+    a = "";
+    b = "";
+    operator = undefined;
+    updateScreen("");
+}
+
+function updateNumber(num) {
+    if (operator === undefined) {
+        a += num;
+    } else {
+        b += num;
+    }
+    console.log("A: ", a);
+    console.log("B: ", b);
+    updateScreen(num);
+}
+
+function updateOperator(symbol) {
+    if (operator == undefined) {
+        operator = symbol;
+    }
+    updateScreen("");
+}
+
+// Handle button press
+button.addEventListener("click", (event) => {
+    const target = event.target;
+    const type = target.dataset;
+
+    if (!target.tagName === "button") return;
+
+    if ("number" in type) {
+        updateNumber(target.textContent);
+    }
+
+    if ("operator" in type) {
+        updateOperator(target.textContent);
+    }
+
+    if ("clear" in type) {
+        clear();
+    }
+
+    if ("equals" in type) {
+        operate(operator,a,b);
+    }
+
+})
+
+let a = "";
+let b = "";
+let operator = undefined;
+
